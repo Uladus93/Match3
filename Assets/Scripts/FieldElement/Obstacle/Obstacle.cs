4604b0528c9c93f29c36fd237aa4e8f357741cfe
Ulad <Loscros88@gmail.com>
2023-11-20 12:45:14 +0300

    create entry point, bonus and obstacle classes
    added some classes.

--- Assets/Scripts/FieldElement/Obstacle/Obstacle.cs
+++ Assets/Scripts/FieldElement/Obstacle/Obstacle.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Token : FieldElement
+public class Obstacle : FieldElement
 {
     private void Awake()
     {
-        _type = TypesOfFieldElements.Token;
+        _type = TypesOfFieldElements.Obstacle;
     }
 }
